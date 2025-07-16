c3ecddb57145850eafb721b185ccc84ccc139a87
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-07-16 19:01:46 +0530

    first commit

---
+++ config/configuration.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, systemSettings, ... }:
+{
+    imports = [];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    networking.hostName = systemSettings.hostname;
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = systemSettings.timezone;
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = systemSettings.locale;
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = systemSettings.locale;
+        LC_IDENTIFICATION = systemSettings.locale;
+        LC_MEASUREMENT = systemSettings.locale;
+        LC_MONETARY = systemSettings.locale;
+        LC_NAME = systemSettings.locale;
+        LC_NUMERIC = systemSettings.locale;
+        LC_PAPER = systemSettings.locale;
+        LC_TELEPHONE = systemSettings.locale;
+        LC_TIME = systemSettings.locale;
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = systemSettings.kbLayout;
+        variant = kbVariant;
+    };
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+
+    };
+
+    # Install firefox.
+    programs.firefox.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+        neovim
+        git
+        gh
+        vscode
+        obsidian
+    ];
+
+    system.stateVersion = "25.05";
+}
---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+    description = "NixOS Dotfiles";
+
+    inputs = {
+        # Nixpkgs
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+        # Home Manager (Standalone Installation)
+        home-manager.url = "github:nix-community/home-manager/master";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = inputs@{ self, ... }:
+    let
+
+        systemSettings = {
+            system = "x86_64-linux";
+            hostname = "nixos";
+            timezone = "Asia/Kolkata";
+            locale = "en_IN";
+            kbLayout = "us";
+            kbVariant = "colemak_dh";
+
+            useSecureboot = false;
+        };
+
+        userSettings = {
+            username = "pranavanath";
+        };
+        
+        pkgs = import inputs.nixpkgs {
+            system = systemSettings.system;
+            config = {
+                allowUnfree = true;
+                allowUnfreePredicate = (_: true);
+            };
+        };
+        lib = inputs.nixpkgs.lib;
+
+    in {
+
+        nixosConfigurations.${systemSettings.hostname} = lib.nixosSystem {
+            system = systemSettings.system;
+            modules = [
+                ./hardware-configuration.nix
+                ./config/configuration.nix
+            ];
+            specialArgs = {
+                inherit inputs;
+                inherit systemSettings;
+                inherit userSettings;
+            };
+        };
+
+        homeConfigurations.${userSettings.username} = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+                ./config/home.nix
+            ];
+            extraSpecialArgs = {
+                inherit inputs;
+                inherit systemSettings;
+                inherit userSettings;
+            };
+        };
+    };
+}
