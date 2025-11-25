b84e20c30b5fec651bb3953ae9cddcba3d334f71
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-11-25 17:54:41 +0530

    commit before switching to niri

--- home/packages/webapps.nix
+++ home/packages/webapps.nix
@@ -6,4 +6,8 @@
         name = "Tasks Google";
         exec = "${lib.getExe pkgs.brave} --app=https://tasks.google.com/tasks/";
     };
+    xdg.desktopEntries.whatsapp = {
+        name = "Whatsapp";
+        exec = "${lib.getExe pkgs.brave} --app=https://web.whatsapp.com/";
+    };
 }
